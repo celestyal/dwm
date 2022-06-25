38dbcf30865cd656c2d1ac2826838d06ef00807c
Alex McNall <asm@alexmcnall.co.uk>
2022-06-25 21:01:25 +0100

    rice: visual change, add fontawesome
    Changes the look of DWM (green colors and thicker borders) and
    adds fontawesome. Fontawesome is used for the icon-like tag labels.

--- config.def.h
+++ config.def.h
@@ -1,28 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Linux Libertine:size=14" };
+static const char *fonts[]          = { "Linux Libertine:size=14", "fontawesome:size=14" };
 static const char dmenufont[]       = "Linux Libertine:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_black[]	    = "#000000"; 
+static const char col_green[]	    = "#00db96";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_green, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_black, col_green, col_green  },
 };
 static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0} }; /* represents the icon as an array of vertices */
 static const XPoint stickyiconbb    = {4,8};	/* defines the bottom right corner of the polygon's bounding box (speeds up scaling) */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -30,10 +31,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
        /* class      instance    title            tags mask  isfloating  isterminal  noswallow  monitor */
-       { "Gimp",     NULL,       NULL,            0,         1,          0,           0,        -1 },
-       { "Firefox",  NULL,       NULL,            1 << 8,    0,          0,          -1,        -1 },
-       { "st-256color",       NULL,       NULL,            0,         0,          1,           0,        -1 },
+       { "Chromium",  NULL,       NULL,            1 << 8,    0,          0,          -1,        -1 },
+       { "st-256color",       	  NULL,       NULL,            0,         0,          1,           0,        -1 },
        { NULL,       NULL,       "Event Tester",  0,         0,          0,           1,        -1 }, /* xev */
+       { "Ymuse",  NULL,       NULL,            1 << 3,    0,          0,          -1,        -1 },
+       { "Thunderbird",  NULL,       NULL,            1 << 5,    0,          0,          -1,        -1 },
 
 };
 
@@ -63,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
