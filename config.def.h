af80004649da4b8ccb977e0a19e27dae6bcbff9c
Alex McNall <asm@alexmcnall.co.uk>
2022-06-29 13:23:21 +0100

    rice: changes to colourscheme, font and tagmasks
    Colourscheme is black and red, the font for dmenu and dwm is Manrope
    Light (requires installation to use) and the tagmask for ymuse is
    removed (no longer needed).

--- config.def.h
+++ config.def.h
@@ -6,18 +6,18 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Linux Libertine:size=14", "fontawesome:size=14" };
-static const char dmenufont[]       = "Linux Libertine:size=14";
+static const char *fonts[]          = { "Manrope Light:size=14", "Linux Libertine:size=14", "fontawesome:size=14" };
+static const char dmenufont[]       = "Manrope Light:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_black[]	    = "#000000"; 
-static const char col_green[]	    = "#00db96";
+static const char col_crimson[]	    = "#dc143c";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_green, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_black, col_green, col_green  },
+	[SchemeNorm] = { col_crimson, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_black, col_crimson, col_crimson  },
 };
 static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0} }; /* represents the icon as an array of vertices */
 static const XPoint stickyiconbb    = {4,8};	/* defines the bottom right corner of the polygon's bounding box (speeds up scaling) */
@@ -33,9 +33,8 @@ static const Rule rules[] = {
        /* class      instance    title            tags mask  isfloating  isterminal  noswallow  monitor */
        { "Chromium",  NULL,       NULL,            1 << 8,    0,          0,          -1,        -1 },
        { "st-256color",       	  NULL,       NULL,            0,         0,          1,           0,        -1 },
-       { NULL,       NULL,       "Event Tester",  0,         0,          0,           1,        -1 }, /* xev */
-       { "Ymuse",  NULL,       NULL,            1 << 3,    0,          0,          -1,        -1 },
        { "Thunderbird",  NULL,       NULL,            1 << 5,    0,          0,          -1,        -1 },
+       { NULL,       NULL,       "Event Tester",  0,         0,          0,           1,        -1 }, /* xev */
 
 };
 
@@ -65,7 +64,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_crimson, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
