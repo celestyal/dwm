de19b9c5afffb3e88ef882c9972ede21e7db703b
Alex McNall <asm@alexmcnall.co.uk>
2022-07-19 19:32:35 +0100

    rice: move bar from top to bottom of display

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Linux Biolinum:size=14", "Linux Libertine:size=14", "fontawesome:size=14" };
 static const char dmenufont[]       = "Linux Biolinum:size=14";
 static const char col_gray1[]       = "#292a2d";
